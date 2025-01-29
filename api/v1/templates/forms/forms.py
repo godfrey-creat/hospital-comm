a87af4b30defefaad686ccce8074a0215c12d169
godfrey-creat <63955844+godfrey-creat@users.noreply.github.com>
2025-01-29 11:22:14 +0300

    Create forms.py

---
+++ api/v1/templates/forms/forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class RegisterAdminForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=100)])
+    submit = SubmitField('Register Admin')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class CreateDepartmentForm(FlaskForm):
+    name = StringField('Department Name', validators=[DataRequired(), Length(max=100)])
+    submit = SubmitField('Create Department')
+
+class AddUserToDepartmentForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=100)])
+    submit = SubmitField('Add User')
+
+class AssignDepartmentHeadForm(FlaskForm):
+    user_id = StringField('User ID', validators=[DataRequired()])
+    submit = SubmitField('Assign Head')
+
+class SendMessageForm(FlaskForm):
+    content = TextAreaField('Message Content', validators=[DataRequired(), Length(max=500)])
+    submit = SubmitField('Send Message')
+
+class CreateLoginsForm(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(), Length(min=4, max=25)
+    ])
+    password = PasswordField('Password', validators=[
+        DataRequired(), Length(min=6)
+    ])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(), EqualTo('password', message='Passwords must match')
+    ])
+    submit = SubmitField('Create Logins')
